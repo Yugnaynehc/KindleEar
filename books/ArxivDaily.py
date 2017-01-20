22ee5636174a15de77a8552e3a019948e0d0bf78
Yugnaynehc <cyyyyc123@hotmail.com>
2017-01-20 10:12:57 +0800

    update

--- books/ArxivDaily.py
+++ books/ArxivDaily.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import datetime
-import requests
+import urllib2
 from lib import feedparser
 from base import BaseFeedBook
 from lib.urlopener import URLOpener
@@ -35,15 +35,17 @@ class ArxivDaily(BaseFeedBook):
         for feed in self.feeds:
             section, url = feed[0], feed[1]
             isfulltext = feed[2] if len(feed) > 2 else False
-            result = requests.get(url)
-            if result.status_code == 200 and result.content:
-                content = result.content.decode(self.feed_encoding)
+            # result = requests.get(url)
+            result = urllib2.urlopen(url)
+            content = result.read()
+            if result.get_code() == 200 and content:
+                content = content.decode(self.feed_encoding)
                 feed = feedparser.parse(content)
-                for e in feed['entries'][:self.max_articles_per_feed]:
+                for e in feed['entries']:
                     authors = e.author
                     arxiv_abs = e.link
                     desc = e.summary
-                    article = authors + arxiv_abs + desc
+                    article = '<p>%s</p><p>%s</p>%s' % (authors, arxiv_abs, desc)
                     urls.append((section, e.title, arxiv_abs, article))
                     urladded.add(arxiv_abs)
             else:
